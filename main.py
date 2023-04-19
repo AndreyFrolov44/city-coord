d81d0b6e7a43825d9d96e1cd8011fb5d34557da5
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-04-19 21:58:11 +0300

    delete on_event

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
 
-from database import database
-
 app = FastAPI()
 
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
 
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
