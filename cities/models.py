c75305ec8c64757a7b54976b6a564b781aab0911
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-04-20 18:55:31 +0300

    fix pk

--- cities/models.py
+++ cities/models.py
@@ -6,8 +6,7 @@ from database import metadata
 cities = sqlalchemy.Table(
     "cities",
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("lat", sqlalchemy.DECIMAL(10, 8), primary_key=True),
+    sqlalchemy.Column("lon", sqlalchemy.DECIMAL(11, 8), primary_key=True),
     sqlalchemy.Column("name", sqlalchemy.String),
-    sqlalchemy.Column("lat", sqlalchemy.DECIMAL(10, 8)),
-    sqlalchemy.Column("lon", sqlalchemy.DECIMAL(11, 8)),
 )
--- cities/schemas.py
+++ cities/schemas.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 
 
 class City(BaseModel):
-    id: Optional[int]
     name: str
     lat: float
     lon: float
