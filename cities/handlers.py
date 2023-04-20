392bca560558e0389d81637c74c53b515f2a5e92
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-04-20 18:56:12 +0300

    add cities to database

--- cities/handlers.py
+++ cities/handlers.py
@@ -1,40 +1,67 @@
+from typing import List, Optional
+
+from geopy import Location
 from geopy.adapters import AioHTTPAdapter
-from sqlalchemy import insert
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from geopy.geocoders import Nominatim
+from fastapi import HTTPException, status
 
 from cities.models import cities
 from cities.schemas import City
 
 
-async def get_coord_city(city_name: str) -> dict:
+async def get_locations(city_name: str) -> Optional[List[Location]]:
     async with Nominatim(
-            user_agent="cities",
+            user_agent='cities',
             adapter_factory=AioHTTPAdapter,
     ) as geolocator:
-        location = await geolocator.geocode(city_name)
+        return await geolocator.geocode(
+            {'city': city_name},
+            addressdetails=True,
+            exactly_one=False,
+            language='en',
+            featuretype='city',
+            namedetails=True
+        )
 
-    return {'lat': location.latitude, 'lon': location.longitude}
 
+async def create_city_handler(city_name: str, session: AsyncSession) -> List[City]:
+    locations = await get_locations(city_name)
 
-async def create_city_handler(city_name: str, session: AsyncSession) -> City:
-    coord = await get_coord_city(city_name)
+    if not locations:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='City with this name not found')
 
-    city = City(
-        name=city_name,
-        lat=coord['lat'],
-        lon=coord['lon'],
-    )
+    cities_list = []
+    values_list = []
+    points = []
 
-    values = {**city.dict()}
-    values.pop('id', None)
+    for location in locations:
+        city = City(
+            name=location.address,
+            lat=location.latitude,
+            lon=location.longitude,
+        )
+        values = {**city.dict()}
+        if location.point not in points:
+            points.append(location.point)
+            cities_list.append(city)
+            values_list.append(values)
 
-    query = insert(cities).values(**values)
+    query = insert(cities).values(values_list) \
+        .on_conflict_do_nothing(index_elements=['lat', 'lon']) \
+        .returning(cities.c.name, cities.c.lat, cities.c.lon)
 
     result = await session.execute(query)
+    insert_cities = result.all()
+
     await session.commit()
 
-    city.id = result.inserted_primary_key[0]
+    if not insert_cities:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='The city already exists')
 
-    return city
-
+    return [City(
+                name=city.name,
+                lat=city.lat,
+                lon=city.lon
+            ) for city in insert_cities]
--- cities/routers.py
+++ cities/routers.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,6 +10,6 @@ from database import get_async_session
 router = APIRouter(prefix='/cities', tags=['cities'])
 
 
-@router.post('/create', response_model=City)
-async def create_city(city_name: str, session: AsyncSession = Depends(get_async_session)) -> City:
+@router.post('/create', response_model=List[City])
+async def create_city(city_name: str, session: AsyncSession = Depends(get_async_session)) -> List[City]:
     return await create_city_handler(city_name, session)
