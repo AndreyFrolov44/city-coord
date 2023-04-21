aa7989bf5fcd6790411ea8328ecf6983bb086c4a
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-04-21 16:03:13 +0300

    fix test city

--- tests/test_cities.py
+++ tests/test_cities.py
@@ -69,17 +69,17 @@ async def test_nearest_city(ac: AsyncClient):
     )
     assert response.status_code == 200
     assert len(response.json()) == 2
-    assert response.json()[0] == {
+    assert {
             "name": "Rome, Roma Capitale, Lazio, Italy",
             "lat": 41.8933203,
             "lon": 12.4829321,
             "distance": 93371.20803817
-        }
-    assert response.json()[1] == {
-            "name": "City of San Marino, 47890, San Marino",
-            "lat": 43.9363996,
-            "lon": 12.4466991,
-            "distance": 133887.28384642
-        }
+        } in response.json()
+    assert {
+            "name": "San Marino, Montelaguardia, Perugia, Umbria, 06125, Italy",
+            "lat": 43.1419676,
+            "lon": 12.3839963,
+            "distance": 45753.22711968
+        } in response.json()
 
 
